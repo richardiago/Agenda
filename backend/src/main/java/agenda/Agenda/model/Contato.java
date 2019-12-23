db5c3c907bf7713f0a3e1a8a205654cdab189551
Iago <iagorichard@outlook.com>
2019-12-22 23:38:21 -0300

    classes

---
+++ backend/src/main/java/agenda/Agenda/model/Contato.java
@@ -0,0 +1,38 @@
+package agenda.Agenda.model;
+
+@Entity
+public class Contato {
+
+    @Id
+        @GeneratedValue
+    private Long id;
+
+    vate String email;
+    e String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+    Strin
+     getNo
+
+    l
+    ome = nome;
+
+    t
+    
+
+    
+    String getSenha() {
+
+    lenha 
+     senha;
+
+    
+     
+    
+
+    
+        
+    
---
+++ backend/src/main/java/agenda/Agenda/model/Usuario.java
@@ -0,0 +1,56 @@
+package agenda.Agenda.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+imp
+
+@Entity
+public class Usuario {
+
+    @Id
+    
+
+    vate long id;
+    e String no
+
+    
+    
+
+    void 
+
+    etId(long id) {
+		this.id = id;
+	}
+	publ
+
+    	return nome;
+	}
+	public void setNome(String nome) {
+		
+
+    }
+        ublic Long  turn 
+
+        ublic void setTe
+    	
+
+    }
+        ublic String getEmail() {
+    	
+
+    }
+        ublic void se
+    	
+
+    }
+        ublic String getEnd
+    	
+
+    }
+        ublic void setEn
+    	
+
+    }
+        
+    
