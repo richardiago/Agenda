704b8fb45fd0dcb6cea9f284d39b04287b2ef1b8
Iago <iagorichard@outlook.com>
2019-12-24 21:29:26 -0300

    Classes Rest

---
+++ backend/src/main/java/agenda/Agenda/controller/usuarioRest.java
@@ -0,0 +1,10 @@
+package agenda.Agenda.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = { "/usuarios" })
+public class usuarioRest {
+
+}
