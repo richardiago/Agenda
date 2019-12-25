1dcf630aaeb498cd6d273d0757501b268810245d
Iago <iagorichard@outlook.com>
2019-12-24 22:39:31 -0300

    implementação dos métodos das classes REST

--- backend/src/main/java/agenda/Agenda/controller/contatoRest.java
+++ backend/src/main/java/agenda/Agenda/controller/contatoRest.java
@@ -1,10 +1,57 @@
 package agenda.Agenda.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import agenda.Agenda.model.entity.Contato;
+import agenda.Agenda.service.contatoService;
+
 @RestController
-@RequestMapping(value = { "/usuarios" })
+@RequestMapping(value = { "/contatos" })
 public class contatoRest {
+
+    @Autowired
+    contatoService contatoservice;
+
+    /* Listar todos os contatos */
+    @GetMapping({ "", "/" })
+    public List<Contato> getContatos(HttpSession httpSession) {
+
+        return contatoservice.listarContatos();
+    }
+
+    /* Procurar contato por Id */
+    @GetMapping("/{id}")
+    public Contato getContato(HttpSession httpSession, @PathVariable(value = "id") Long id) {
+
+        return contatoservice.getById(id);
+    }
+
+    /* Criar novo contato */
+    @PostMapping({ "", "/" })
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarContato(@RequestBody Contato u) {
+
+        contatoservice.criarContato(u);
+    }
+
+    /* Excuir contato por Id */
+    @DeleteMapping("/{id}")
+    public void excluiContato(HttpSession httpSession, @PathVariable(value = "id") long id) {
+
+        contatoservice.excluirContato(id);
+    }
 
 }
--- backend/src/main/java/agenda/Agenda/controller/usuarioRest.java
+++ backend/src/main/java/agenda/Agenda/controller/usuarioRest.java
@@ -1,10 +1,57 @@
 package agenda.Agenda.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import agenda.Agenda.model.entity.Usuario;
+import agenda.Agenda.service.usuarioService;
 
 @RestController
 @RequestMapping(value = { "/usuarios" })
 public class usuarioRest {
+
+    @Autowired
+    usuarioService usuarioservice;
+
+    /* Listar todos os usuários */
+    @GetMapping({ "", "/" })
+    public List<Usuario> getUsuarios(HttpSession httpSession) {
+
+        return usuarioservice.listarUsuarios();
+    }
+
+    /* Procurar usuario por Id */
+    @GetMapping("/{id}")
+    public Usuario getUsuario(HttpSession httpSession, @PathVariable(value = "id") Long id) {
+
+        return usuarioservice.getById(id);
+    }
+
+    /* Criar novo usuario */
+    @PostMapping({ "", "/" })
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarUsuario(@RequestBody Usuario u) {
+
+        usuarioservice.criarUsuario(u);
+    }
+
+    /* Excuir usuario por Id */
+    @DeleteMapping("/{id}")
+    public void excluiUsuario(HttpSession httpSession, @PathVariable(value = "id") long id) {
+
+        usuarioservice.excluirUsuario(id);
+    }
 
 }
